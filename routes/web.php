de02f6e1a1ab1cab7ab2a9ce166324647ab34c1f
alejandroguzman23221 <alejandroguzman23221@gmail.com>
2023-03-16 15:02:30 -0600

    otro

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Verificar;
+use App\Http\Controllers\Edgar;
 use Illuminate\Support\Facades\Route;
+use App\Mail\Correo;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use App\Models\codes;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +26,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/todos', [Edgar::class, 'index2']);
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -27,5 +37,12 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('thor', 
+[Edgar::class, 'create']);
+
+Route::get('/createcode/{user}',[Verificar::class, 'create'])->name('unsubscribe');
+Route::post('/validate-code-web', [Verificar::class, 'storeWeb']);
+
 
 require __DIR__.'/auth.php';
